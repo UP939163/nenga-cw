49e5992fe211fc530bdadf1bff2b2d79d48b0e1d
UP939163 <72736543+UP939163@users.noreply.github.com>
2022-04-27 11:53:43 +0100

    Randomise weights

--- Task 1/nn.m
+++ Task 1/nn.m
@@ -35,6 +35,11 @@ net.divideParam.trainRatio = 70/100;
 net.divideParam.valRatio = 15/100;
 net.divideParam.testRatio = 15/100;
 
+% Randomise Weights
+rng(0)
+net = configure(net,x,t);
+net = setwb(net, rand(35,1));
+
 % Train the Network
 [net,tr] = train(net,x,t);
 
